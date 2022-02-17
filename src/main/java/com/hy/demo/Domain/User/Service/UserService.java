7c0a9d46d9cc4abec75f3d26368a478593c003e6
ddh963963 <ddh963963@naver.com>
2022-02-17 21:54:30 +0900

    fix : 로그인 회원가입 뒤로가기 방지 fix
    oauth 회원가입도중 뒤로가기 나 페이지 새로고침시에 자동으로 로그인되는 버그 수정

--- src/main/java/com/hy/demo/Domain/User/Service/UserService.java
+++ src/main/java/com/hy/demo/Domain/User/Service/UserService.java
@@ -2,11 +2,20 @@ package com.hy.demo.Domain.User.Service;
 
 import com.hy.demo.Domain.User.Entity.User;
 import com.hy.demo.Domain.User.Repository.UserRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 @Service
 public class UserService {
@@ -14,20 +23,23 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
+    @Transactional
     public void register(User user,User provider) {
-
-
         if (provider == null) {
             user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
             logger.info("user.getPassword() = " + user.getPassword());
             userRepository.save(user);
         } else {
+
+
+
             User user2 = User.builder()
                     .username(provider.getUsername())
                     .password(provider.getPassword())
@@ -37,13 +49,30 @@ public class UserService {
                     .providerId(provider.getProviderId())
                     .build();
             logger.info("user.getPassword() = " + user2.getPassword());
+
             userRepository.save(user2);
+
         }
 
 
-
-
-
     }
+
+
+
+
+    public Boolean loginForm(User user) {
+
+
+        String username = user.getUsername();
+        User findUser = userRepository.findByUsername(username);
+        if (findUser != null) {
+            logger.info("회원");
+            return true;
+        }else {
+            logger.info("비회원");
+            return false;
+        }
+    }
+
 
 }
