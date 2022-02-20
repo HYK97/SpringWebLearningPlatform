097837b7f80bc16f6bbdf76da8e35e425c1a66cd
ddh963963 <ddh963963@naver.com>
2022-02-20 20:31:38 +0900

    Fix : OAuth 로그인후 리다이렉션 주소변경, mainindex 세션 확인 변경
    기존의 로그인후 loginForm으로 리다이렉션되던 걸 /main/index로 되게 변경
    Test를 하기위해 Authentication으로 매개변수 변경

--- src/main/java/com/hy/demo/Domain/Board/Controller/MainController.java
+++ src/main/java/com/hy/demo/Domain/Board/Controller/MainController.java
@@ -6,6 +6,7 @@ import com.hy.demo.Utils.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,9 +26,9 @@ public class MainController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping( {"/index"})
-    public String index(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+    public String index(Authentication authentication) {
 
-        if (!isEmpty(principalDetails)) {
+        if (!isEmpty(authentication)) {
             return "/main/index";
         }else
             return "/loginForm";
