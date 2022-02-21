2b91b642428aa3a1ae36ecf9148b356965b2c958
KANBi kim <ddh963963@gmail.com>
2022-02-21 20:27:58 +0900

    Test : joinPost Test 추가
    post 타입 join 컨트롤러 테스트 추가
    400 뜨는지 확인해야함

--- src/test/java/com/hy/demo/Domain/Board/Controller/MainControllerTest.java
+++ src/test/java/com/hy/demo/Domain/Board/Controller/MainControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.omg.PortableInterceptor.SUCCESSFUL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
