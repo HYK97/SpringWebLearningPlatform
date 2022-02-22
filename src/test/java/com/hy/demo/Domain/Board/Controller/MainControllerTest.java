0c639f25abb6c1443b341034e678f42f0c81d2be
ddh963963 <ddh963963@naver.com>
2022-02-23 00:14:28 +0900

    Refactor : 메인페이지 principalDetails 형식으로 변경후 Test issue 발생
    issue : PostConstruct 두번 실행됌

--- src/test/java/com/hy/demo/Domain/Board/Controller/MainControllerTest.java
+++ src/test/java/com/hy/demo/Domain/Board/Controller/MainControllerTest.java
@@ -1,39 +1,32 @@
 package com.hy.demo.Domain.Board.Controller;
 
-import com.hy.demo.Config.Auth.PrincipalDetails;
 import com.hy.demo.Domain.User.Entity.User;
 import com.hy.demo.Domain.User.Repository.UserRepository;
-import org.junit.After;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-
 class MainControllerTest {
 
     @Autowired
@@ -42,15 +35,29 @@ class MainControllerTest {
     @Autowired
     private UserRepository userRepository;
 
-
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    private Long testCode;
 
     private MockMvc mvc;
+
+    @PostConstruct
+
+    public void accountSetup() {
+        testCode = userRepository.save(User.builder()
+                .username("test")
+                .email("test@com")
+                .role("ROLE_USER")
+                .password("password").build()).getId();
+    }
+
+
+
+
 
     @BeforeEach
     public void setup(){
@@ -59,8 +66,44 @@ class MainControllerTest {
                 .apply(springSecurity())
                 .build();
 
+        User user = User.builder()
+                .username("user")
+                .role("ROLE_USER")
+                .email("user@gmail.com")
+                .password(passwordEncoder.encode("user"))
+                .build();
+        User manager = User.builder()
+                .username("manager")
+                .role("ROLE_MANAGER")
+                .email("manager@gmail.com")
+                .password(passwordEncoder.encode("manager"))
+                .build();
+        User admin = User.builder()
+                .username("admin")
+                .role("ROLE_ADMIN")
+                .email("admin@gmail.com")
+                .password(passwordEncoder.encode("admin"))
+                .build();
+
+
+        userRepository.save(user);
+        userRepository.save(manager);
+        userRepository.save(admin);
+
     }
 
+
+    @Test
+    @WithUserDetails(value = "test")
+    public void indexSuccessTest() throws Exception {
+
+        // when
+        mvc.perform(get("/main/index"))
+                .andDo(print())
+        // then
+                .andExpect(status().isOk())
+                .andExpect(view().name("/main/index"));
+    }
 
     @Test
     @WithAnonymousUser
@@ -76,18 +119,7 @@ class MainControllerTest {
 
     }
 
-    @WithMockUser
-    @Test
-    public void indexSuccessTest() throws Exception {
 
-        // when
-        mvc.perform(get("/main/index"))
-                .andDo(print())
-
-        // then
-                .andExpect(status().isOk())
-                .andExpect(view().name("/main/index"));
-    }
 
 
 
