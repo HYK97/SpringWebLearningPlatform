b5e2f412c4be4a61675f76b3375676b4507389ed
ddh963963 <ddh963963@naver.com>
2022-02-24 22:12:31 +0900

    Feat : courseSearch 및 view 컨트롤러 추가
    회원이 Course(강의)를 추가했을때 view에 보이도록 수정

--- src/main/java/com/hy/demo/Domain/Board/Controller/CourseController.java
+++ src/main/java/com/hy/demo/Domain/Board/Controller/CourseController.java
@@ -9,16 +9,22 @@ import com.hy.demo.Domain.User.Repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.transaction.Transactional;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 @Controller
@@ -31,19 +37,54 @@ public class CourseController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping( {"/view"})
-    @ResponseBody
-    public List<CourseDto> course(Pageable pageable) {
-        System.out.println("courseService.viewCourse(pageable) = " + courseService.viewCourse(pageable).toString());
-        return courseService.viewCourse(pageable);
+        public String course(@PageableDefault(size = 9, sort = "createDate", direction = Sort.Direction.DESC) Pageable pageable, Model model) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
+
+        Page<CourseDto> courseDtos=courseService.viewCourse(pageable);
+        List<CourseDto> content = courseDtos.getContent();
+        int pageNumber = courseDtos.getPageable().getPageNumber();
+        int totalPages = courseDtos.getTotalPages();
+        boolean Previous = courseDtos.hasPrevious();
+        boolean Next = courseDtos.hasNext();
+        model.addAttribute("course",content);
+        model.addAttribute("pageNumber",pageNumber+1);
+        model.addAttribute("totalPages",totalPages);
+        model.addAttribute("Previous",Previous);
+        model.addAttribute("Next",Next);
+        logger.info("1현재페이지 = " + pageNumber);
+        logger.info("전체페이지 = " + totalPages);
+        logger.info("이전페이지있냐 = " + Previous);
+        logger.info("다음페이지있냐 = " + Next);
+        return "/course/view";
     }
+
+
 
     @GetMapping( {"/search"})
-    @ResponseBody
-    public List<CourseDto> courseSearch(String search,Pageable pageable) {
-        System.out.println("search = " + courseService.searchCourse(search,pageable));
-        return courseService.searchCourse(search,pageable);
+
+    public String courseSearch(Model model,String search,@PageableDefault(size = 9, sort = "createDate", direction = Sort.Direction.DESC)Pageable pageable) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Page<CourseDto> courseDtos=courseService.searchCourse(search,pageable);
+        List<CourseDto> content = courseDtos.getContent();
+        int pageNumber = courseDtos.getPageable().getPageNumber();
+        int totalPages = courseDtos.getTotalPages();
+        boolean Previous = courseDtos.hasPrevious();
+        boolean Next = courseDtos.hasNext();
+        model.addAttribute("course",content);
+        model.addAttribute("pageNumber",pageNumber+1);
+        model.addAttribute("totalPages",totalPages);
+        model.addAttribute("Previous",Previous);
+        model.addAttribute("Next",Next);
+        logger.info("1현재페이지 = " + pageNumber);
+        logger.info("전체페이지 = " + totalPages);
+        logger.info("이전페이지있냐 = " + Previous);
+        logger.info("다음페이지있냐 = " + Next);
+
+        return "/course/view";
+
     }
+
+
 
 
 
@@ -56,7 +97,7 @@ public class CourseController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Transactional
+
     @GetMapping( {"/create"})
     public String testCreate(@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
@@ -67,8 +108,14 @@ public class CourseController {
                 .user(user)
                 .heart(0)
                 .build();
+        Course course2 = Course.builder()
+                .courseName("asd")
+                .user(user)
+                .heart(0)
+                .build();
 
         courseService.createCourse(course);
+        courseService.createCourse(course2);
         return "/course/view";
     }
 }
