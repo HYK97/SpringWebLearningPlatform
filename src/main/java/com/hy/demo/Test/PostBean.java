ccab13026c92074e47421af2e42a850c359b3762
ddh963963 <ddh963963@naver.com>
2022-02-24 16:58:04 +0900

    Feat : 테스트용 유저 자동 생성

---
+++ src/main/java/com/hy/demo/Test/PostBean.java
@@ -0,0 +1,34 @@
+package com.hy.demo.Test;
+
+import com.hy.demo.Domain.User.Entity.User;
+import com.hy.demo.Domain.User.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostBean  implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        //테스트유저
+        User user = User.builder()
+                .username("tuser")
+                .role("ROLE_USER")
+                .email("user@gmail.com")
+                .password(passwordEncoder.encode("user"))
+                .build();
+
+        userRepository.save(user);
+    }
+}
