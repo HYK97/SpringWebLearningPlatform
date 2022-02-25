0b39738b281e3819720124a5fb5db246e2bbea56
ddh963963 <ddh963963@naver.com>
2022-02-26 06:27:06 +0900

    Feat : header 로그인된사람의 role에 따라서 drop메뉴 변경
    user는 수강중인 강의만 헤더에 보이도록
    manager 이상부터는 강좌 생성되도록 변경

--- src/main/resources/static/assets/dist/js/header.js
+++ src/main/resources/static/assets/dist/js/header.js
@@ -1,5 +1,21 @@
+$(document).ready(function() {
 
-    $(document).ready(function() {
+    $.ajax({
+        type : "post",
+        url : "/user/role",
+        cache : false,
+        success : function(data){
+            if (data == 'ROLE_USER') {
+                $('#manager').remove();
+            } else {
+                $('#user').after('<li><a id="manager" class="dropdown-item" href="/course/createview">강의 생성</a></li>');
+            }
+        },
+        error:function(request, error) {
+            alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+            alert("세션오류");
+        }
+    });
 
-        /*{{user.}}*/
+
 });
