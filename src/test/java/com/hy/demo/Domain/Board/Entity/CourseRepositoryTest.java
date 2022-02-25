855dc023f7227136564fe4b2640d7386398d925a
ddh963963 <ddh963963@naver.com>
2022-02-25 21:24:07 +0900

    Feat : view에서 강사의 id가 아닌 실명이 나오도록 구조를 변경함
    기존 -> 강사의 아이디 즉 userid를 통함
    
    변경후 -> 강좌를 만들때 강사가 특정 닉네임을 사용할수있도록 변경함
    
    이에따른 검색 로직과 테스트 repository를 변경함

--- src/test/java/com/hy/demo/Domain/Board/Entity/CourseRepositoryTest.java
+++ src/test/java/com/hy/demo/Domain/Board/Entity/CourseRepositoryTest.java
@@ -56,23 +56,27 @@ class CourseRepositoryTest {
         Course course1 = Course.builder()
                 .courseName("test1")
                 .user(manager1)
+                .teachName("manager1")
                 .heart(0)
                 .build();
 
         Course course2 = Course.builder()
                 .courseName("test2")
                 .user(manager1)
+                .teachName("manager1")
                 .heart(0)
                 .build();
 
         Course course3 = Course.builder()
                 .courseName("test3")
                 .user(manager2)
+                .teachName("manager2")
                 .heart(0)
                 .build();
         Course course4 = Course.builder()
                 .courseName("english")
                 .user(manager2)
+                .teachName("manager2")
                 .heart(0)
                 .build();
 
@@ -88,25 +92,33 @@ class CourseRepositoryTest {
 
     @Test
     public void searchCourse() {
+
         //강좌명으로검색
         AssertCourse(0,3,"test",3,"User","username",new String[] {"manager1","manager1","manager2"},"courseName",new String[] {"test1","test2","test3"});
         AssertCourse(0,2,"test",2,"User","username",new String[] {"manager1","manager1"},"courseName",new String[] {"test1","test2"});
         AssertCourse(0,3,"english",1,"User","username",new String[] {"manager2"},"courseName",new String[] {"english"});
         AssertCourse(0,3,"false",0,"User","username",new String[]{},"courseName",new String[]{});
         AssertCourse(0,3,"",3,"User","username",new String[] {"manager1","manager1","manager2"},"courseName",new String[] {"test1","test2","test3"});
+
         //강사명으로 검색
-        AssertCourse(0,3,"manager2",2,"User","username",new String[]{"manager2","manager2"},"courseName",new String[]{"english","test3"});
-        AssertCourse(0,3,"manager1",2,"User","username",new String[]{"manager1","manager1"},"courseName",new String[]{"test1","test2"});
+        AssertCourse(0,3,"manager2",2,"teachName","",new String[]{"manager2","manager2"},"courseName",new String[]{"english","test3"});
+        AssertCourse(0,3,"manager1",2,"teachName","",new String[]{"manager1","manager1"},"courseName",new String[]{"test1","test2"});
     }
 
     private void AssertCourse(int page,int size,String courseName,int resultSize,String extracting1,String extracting2,String []contains1,String extracting3,String []contains2) {
         PageRequest pageRequest = PageRequest.of(page, size);
         Page<CourseDto> findCourse = courseRepository.findByCourseNameAndUserDTO(courseName, pageRequest);
         assertThat(findCourse.getContent().size()).isEqualTo(resultSize);
-        assertThat(findCourse.getContent())
-                .extracting(extracting1)
-                .extracting(extracting2)
-                .contains(contains1);
+        if (extracting2.equals("")) {
+            assertThat(findCourse.getContent())
+                    .extracting(extracting1)
+                    .contains(contains1);
+        } else {
+            assertThat(findCourse.getContent())
+                    .extracting(extracting1)
+                    .extracting(extracting2)
+                    .contains(contains1);
+        }
         assertThat(findCourse.getContent())
                 .extracting(extracting3)
                 .contains(contains2);
