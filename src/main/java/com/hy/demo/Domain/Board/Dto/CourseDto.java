a49139ce49ef05ed3e3abee5ce61cdeecb90e6c8
ddh963963 <ddh963963@naver.com>
2022-02-27 19:09:13 +0900

    Feat : 해당 게시글의 별점 평균과 분포도를 볼수있도록 추가함
    게시글의 id를 가져와서 해당 게시글의 리뷰 평점을 계산하여 view로 보여주도록 함

--- src/main/java/com/hy/demo/Domain/Board/Dto/CourseDto.java
+++ src/main/java/com/hy/demo/Domain/Board/Dto/CourseDto.java
@@ -1,19 +1,12 @@
 package com.hy.demo.Domain.Board.Dto;
 
-import com.hy.demo.Domain.BaseEntity;
-import com.hy.demo.Domain.Board.Entity.Comments;
 import com.hy.demo.Domain.Board.Entity.Course;
-import com.hy.demo.Domain.Board.Entity.CourseBoard;
-import com.hy.demo.Domain.Board.Entity.Notice;
 import com.hy.demo.Domain.User.Entity.User;
-import com.hy.demo.Domain.User.Entity.UserCourse;
 import com.hy.demo.Utils.ObjectUtils;
 import lombok.*;
 
-import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -37,8 +30,13 @@ public class CourseDto  {
 
     private Double scope;
 
-    private Double starscope;
+    private Double starScope;
 
+    private Long reviewCount;
+
+    private int[] score= new int[]{5, 4, 3, 2, 1};
+
+    private List<Double> starPercent;
 
     public Course returnEntity() {
         return Course.builder()
@@ -53,7 +51,7 @@ public class CourseDto  {
         this.user =new CourseUser(user.getUsername(),user.getEmail(),user.getRole());
     }
 
-    public CourseDto(Long id, String courseName, User user, Timestamp createDate, String teachName, String thumbnail, String courseExplanation, Double scope) {
+    public CourseDto(Long id, String courseName, User user, Timestamp createDate, String teachName, String thumbnail, String courseExplanation, Double scope,Long reviewCount) {
         this.id = id;
         this.courseName = courseName;
         this.user =new CourseUser(user.getUsername(),user.getEmail(),user.getRole());
@@ -63,12 +61,13 @@ public class CourseDto  {
         this.courseExplanation =courseExplanation;
         if (!ObjectUtils.isEmpty(scope)) {
             this.scope = (Math.round(scope*10)/10.0);
-            this.starscope =this.scope*20;
-            this.starscope +=1.5;
+            this.starScope =this.scope*20;
+            this.starScope +=1.5;
         } else {
             this.scope = 0.0;
-            this.starscope =0.0;
+            this.starScope =0.0;
         }
+        this.reviewCount=reviewCount;
 
     }
 
--- src/main/java/com/hy/demo/Domain/Board/Service/CourseService.java
+++ src/main/java/com/hy/demo/Domain/Board/Service/CourseService.java
@@ -2,19 +2,29 @@ package com.hy.demo.Domain.Board.Service;
 
 import com.hy.demo.Domain.Board.Dto.CourseDto;
 import com.hy.demo.Domain.Board.Entity.Course;
+import com.hy.demo.Domain.Board.Repository.CourseEvaluationRepository;
+import com.hy.demo.Domain.Board.Repository.CourseEvaluationRepositoryCustom;
 import com.hy.demo.Domain.Board.Repository.CourseRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CourseService {
 
     @Autowired
     private CourseRepository courseRepository;
+
+    @Autowired
+    Logger logger;
+    @Autowired
+    private CourseEvaluationRepository courseEvaluationRepository;
 
     public Page<CourseDto> viewCourse(Pageable pageable) {
 
@@ -24,7 +34,21 @@ public class CourseService {
 
     public CourseDto detailView(Long id) {
 
-        CourseDto results = courseRepository.findByIdAndUserDTO(id);
+        CourseDto results = courseRepository.findByIdAndUserDTO(id);//
+        //별점갯수
+        Map<String, Double> countScope = courseEvaluationRepository.countScope(id);
+        Double allCount = 0.0;
+        for (String key : countScope.keySet()) {
+            allCount+=countScope.get(key);
+        }
+        List<Double> percent =new ArrayList<>();
+        for (int i = 1; i <6 ; i++) {
+            percent.add(countScope.get(Integer.toString(i))/allCount*100);
+        }
+        for (Double aDouble : percent) {
+           logger.info("aDouble = " + aDouble);
+        }
+        results.setStarPercent(percent);
 
         return results;
     }
