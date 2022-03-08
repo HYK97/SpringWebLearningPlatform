74d61cb0214a9275038ac73de2f84a854ae5f0d3
ddh963963 <ddh963963@naver.com>
2022-03-08 17:06:26 +0900

    Feat : 수강평작성값 유효성확인 추가

--- src/main/resources/static/assets/dist/js/form-validation.js
+++ src/main/resources/static/assets/dist/js/form-validation.js
@@ -29,3 +29,29 @@ function fileCheck(el) {
     el.focus();
   }
 }
+
+
+function formCheck(form) {
+
+  let star = $(form).find('input:radio[name=star]').is(':checked');
+  let text = $(form).find('.form-control').val();
+  let textBoolean;
+  // 미입력 또는 공백 입력 방지
+  if (text.replace(/\s|　/gi, "").length == 0) {
+    $("#modal-message-text").focus();
+    textBoolean = false;
+  } else {
+    textBoolean = true;
+  }
+
+    return star && textBoolean;
+
+
+
+
+}
+
+
+
+
+
