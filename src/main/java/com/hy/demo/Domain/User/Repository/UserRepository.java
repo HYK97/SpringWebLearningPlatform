a679e1abd07e1d36f1b479f8ec952ea1d43809c5
ddh963963 <ddh963963@naver.com>
2022-03-10 15:24:09 +0900

    Refactor : Test 데이터 변경 및 테스트 이름변경

--- src/main/java/com/hy/demo/Domain/User/Repository/UserRepository.java
+++ src/main/java/com/hy/demo/Domain/User/Repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.hy.demo.Domain.User.Repository;
 import com.hy.demo.Domain.User.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
     //findBy규칙 -> Username문법
     User findByUsername(String username);
     void deleteById(Long id);
+    List<User> findByRole(String role);
 }
