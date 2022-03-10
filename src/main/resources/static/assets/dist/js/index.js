805d450b3f5ad84a33cad959f7b05e8affd6435e
ddh963963 <ddh963963@naver.com>
2022-03-10 18:40:29 +0900

    Feat : random course view 기능 추가

---
+++ src/main/resources/static/assets/dist/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+    $('.btn0').addClass("active");
+    $('.btn0').attr("aria-current","true");
+    $('.carousel-inner').children().first().addClass("active");
+
+
+});
+
+
+
