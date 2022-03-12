8ea3152e26890cb699d107f63cf4532b5b82fa27
ddh963963 <ddh963963@naver.com>
2022-03-13 01:50:24 +0900

    Feat : 수강신청을 하지않은 학생은 수강신청페이지로 이동하는 기능 추가
    @PreAuthorize 어노테이션을 사용해 check 를 사용해서 해당사용자가 수강신청을 한사용자인지 확인한다.

--- src/main/java/com/hy/demo/Domain/Board/Controller/CourseBoardController.java
+++ src/main/java/com/hy/demo/Domain/Board/Controller/CourseBoardController.java
@@ -4,20 +4,21 @@ package com.hy.demo.Domain.Board.Controller;
 import com.hy.demo.Domain.Board.Dto.CourseBoardDto;
 import com.hy.demo.Domain.Board.Entity.CourseBoard;
 import com.hy.demo.Domain.Board.Service.CourseBoardService;
-import com.hy.demo.Domain.Course.Service.CourseService;
-import com.hy.demo.Domain.Course.Service.ImageService;
-import com.hy.demo.Domain.User.Service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-;import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+;
 
 @Controller
 @RequestMapping("/courseBoard/*")
@@ -31,11 +32,27 @@ public class CourseBoardController {
 
 
     @GetMapping("/{id}")
-    public String viewBoardList(Model model, @PathVariable Long id) {
+    @PreAuthorize("@authorizationChecker.isAccessBoard(#id)")
+    public String viewBoardList(Model model, @PathVariable Long id){
+
         List<CourseBoardDto> courseBoardList = courseBoardService.findCourseBoardList(id);
         model.addAttribute("courseList",courseBoardList);
+
         return "/courseboard/view";
     }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ModelAndView handleAccessDeniedException(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String requestURI = request.getRequestURI().trim();
+        String[] split = requestURI.split("/");
+        ModelAndView model =new ModelAndView();
+        logger.info("requestURI = " + split[2]);
+        model.addObject("id",split[2]);
+        model.addObject("joinBtn",true);
+        model.setViewName("redirect:/course/detailcourse?");
+        return  model;
+    }
+
     @GetMapping("/createCourseBoard")
     @ResponseBody
     public void createCourseBoard(CourseBoard courseBoard) {
