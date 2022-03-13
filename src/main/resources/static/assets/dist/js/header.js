9b7ed539856bf0dbd26778d3ce420befe8094339
ddh963963 <ddh963963@naver.com>
2022-03-13 17:49:07 +0900

    Feat : 강의 관리 view 생성 및 import resource 동적경로 변경

--- src/main/resources/static/assets/dist/js/header.js
+++ src/main/resources/static/assets/dist/js/header.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
                 $('#manager').remove();
             } else {
                 $('#user').after('<li><a id="manager" class="dropdown-item" href="/course/createview">강의 생성</a></li>');
+                $('#user').after('<li><a id="manager" class="dropdown-item" href="/courseboard/info/mycourselist">강의관리</a></li>');
+
             }
         },
         error:function(request, error) {
