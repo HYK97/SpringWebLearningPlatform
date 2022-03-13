da32f369bc9f088b65119c63a7f7f7e301a526c4
ddh963963 <ddh963963@naver.com>
2022-03-14 01:37:51 +0900

    Refactor : courseDto querydsl 용 생성자 추가 및 findByUserIdAndCourseName 메소드 추가
    repository - findByUserIdAndCourseName 메소드르 추가해서 유저아이디로 나의 강의 찾기 쿼리를 날리도록함
    이에 필요한 Projection을 하기위해 courseDto field 수정을 함 또 기존 쿼리 겹치는 부분을 메소드로 따로 빼는 작업을함

--- src/main/java/com/hy/demo/Domain/Course/Repository/CourseRepositoryImpl.java
+++ src/main/java/com/hy/demo/Domain/Course/Repository/CourseRepositoryImpl.java
@@ -1,25 +1,35 @@
 package com.hy.demo.Domain.Course.Repository;
 
+import com.hy.demo.Domain.Board.Entity.QCourseBoard;
 import com.hy.demo.Domain.Course.Dto.CourseDto;
 import com.hy.demo.Domain.Course.Entity.Course;
 import com.hy.demo.Domain.User.Entity.QUser;
+import com.hy.demo.Domain.User.Entity.QUserCourse;
 import com.hy.demo.Utils.QueryDsl4RepositorySupport;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQuery;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+import static com.hy.demo.Domain.Board.Entity.QCourseBoard.courseBoard;
 import static com.hy.demo.Domain.Course.Entity.QCourse.course;
 import static com.hy.demo.Domain.Course.Entity.QCourseEvaluation.courseEvaluation;
 import static com.hy.demo.Domain.User.Entity.QUser.user;
+import static com.hy.demo.Domain.User.Entity.QUserCourse.*;
+import static com.querydsl.core.types.ExpressionUtils.count;
 
 
-public class CourseRepositoryImpl extends QueryDsl4RepositorySupport implements CourseRepositoryCustom  {
+public class CourseRepositoryImpl extends QueryDsl4RepositorySupport implements CourseRepositoryCustom {
 
 
+    @Autowired
+    Logger logger;
 
     public CourseRepositoryImpl() {
         super(Course.class);
@@ -27,14 +37,39 @@ public class CourseRepositoryImpl extends QueryDsl4RepositorySupport implements 
 
     public Page<Course> findByCourseNameAndUser(String courseName, Pageable pageable) {
 
-        return applyPagination(pageable,query ->
-                        query.select(course)
+        return applyPagination(pageable, query ->
+                query.select(course)
                         .from(course)
                         .leftJoin(course.user, QUser.user)
                         .fetchJoin()
                         .where(course.courseName.contains(courseName))
-                    );
+        );
     }
+
+
+    public Page<CourseDto> findByUserIdAndCourseName(String courseName, Long userId, Pageable pageable) {
+        return applyPagination(pageable, query -> select(Projections.constructor(CourseDto.class
+                , course.id
+                , course.courseName
+                , course.createDate
+                , course.teachName
+                , select(courseEvaluation.scope.avg().as("scope")).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
+                , select(userCourse.course.count().as("userJoinCount")).from(userCourse).where(userCourse.course.id.eq(course.id)).groupBy(userCourse.course.id)
+                ))
+                        .from(course)
+                        .leftJoin(course.user, user)
+                        .where(userIdeEq(userId), courseNameContains(courseName))
+        );
+    }
+
+    private BooleanExpression userIdeEq(Long userId) {
+        return userId != null ? course.user.id.eq(userId) : null;
+    }
+
+    private BooleanExpression courseNameContains(String courseName) {
+        return courseName != null ? course.courseName.contains(courseName) : null;
+    }
+
 
     public Page<CourseDto> findByCourseNameAndUserDTO(String courseName, Pageable pageable) {
         return applyPagination(pageable, query ->
@@ -43,40 +78,21 @@ public class CourseRepositoryImpl extends QueryDsl4RepositorySupport implements 
         );
     }
 
+
     public CourseDto findByIdAndUserDTO(Long id) {
         return getCourseDtoJPAQuery()
-                        .where(course.id.eq(id)).fetchOne();
+                .where(course.id.eq(id)).fetchOne();
     }
 
     private JPAQuery<CourseDto> getCourseDtoJPAQuery() {
-        return select(Projections.constructor(CourseDto.class
-                , course.id
-                , course.courseName
-                , user
-                , course.createDate
-                , course.teachName
-                , course.thumbnail
-                , course.courseExplanation
-                , select(courseEvaluation.scope.avg()).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
-                , select(courseEvaluation.scope.count()).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
-        ))
+        return getSelectConstructorCourseDto()
                 .from(course)
                 .leftJoin(course.user, user);
     }
 
 
     public List<CourseDto> findByRandomId(int amount) {
-        return select(Projections.constructor(CourseDto.class
-                , course.id
-                , course.courseName
-                , user
-                , course.createDate
-                , course.teachName
-                , course.thumbnail
-                , course.courseExplanation
-                , select(courseEvaluation.scope.avg()).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
-                , select(courseEvaluation.scope.count()).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
-        ))
+        return getSelectConstructorCourseDto()
                 .from(course)
                 .leftJoin(course.user, user)
                 .orderBy(NumberExpression.random().asc())
@@ -84,7 +100,20 @@ public class CourseRepositoryImpl extends QueryDsl4RepositorySupport implements 
                 .fetch();
     }
 
-
-
+    private JPAQuery<CourseDto> getSelectConstructorCourseDto() {
+        return select(Projections.constructor(CourseDto.class
+                , course.id
+                , course.courseName
+                , user
+                , course.createDate
+                , course.teachName
+                , course.thumbnail
+                , course.courseExplanation
+                , select(courseEvaluation.scope.avg()).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
+                , select(courseEvaluation.scope.count()).from(courseEvaluation).where(courseEvaluation.course.id.eq(course.id)).groupBy(courseEvaluation.course.id)
+        ));
     }
+
+
+}
 
