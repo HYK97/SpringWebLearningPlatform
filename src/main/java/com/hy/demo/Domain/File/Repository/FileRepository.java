d528d1eff5b7fb2142dda9cfcd3a10147e526bf9
ddh963963 <ddh963963@naver.com>
2022-03-17 21:03:58 +0900

    Feat : File Repository 및 service 추가

---
+++ src/main/java/com/hy/demo/Domain/File/Repository/FileRepository.java
@@ -0,0 +1,15 @@
+package com.hy.demo.Domain.File.Repository;
+
+import com.hy.demo.Domain.Course.Entity.Course;
+import com.hy.demo.Domain.File.Entity.File;
+import com.hy.demo.Domain.User.Entity.User;
+import com.hy.demo.Domain.User.Entity.UserCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FileRepository extends JpaRepository<File,Long> {
+
+
+
+}
