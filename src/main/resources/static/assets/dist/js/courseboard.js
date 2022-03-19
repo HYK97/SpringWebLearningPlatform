ba2b9f472289946e9869e7ebc07f72f61ee391ca
ddh963963 <ddh963963@naver.com>
2022-03-19 21:09:39 +0900

    Feat : 첨부파일 기능 추가 및 view render 형식 model 에서 ajax 로변경
    기존의 컨트롤러에서 view model 로 가져오지않고 mustache를 이용해 ajax 통신후 데이터를 렌더링하도록함
    
    첨부파일 기능을 추가함

--- src/main/resources/static/assets/dist/js/courseboard.js
+++ src/main/resources/static/assets/dist/js/courseboard.js
@@ -1,5 +1,5 @@
 const template =' ' +
-    ' {{#data.CourseBoardData}}' +
+    ' {{#data}}' +
     '<main class="col-md-12 ms-sm-auto col-lg-12  px-md-4">\n' +
     '\n' +
     '\n' +
@@ -11,37 +11,61 @@ const template =' ' +
     '                    </div>\n' +
     '                </div>\n' +
     '\n' +
-    '                <div class="my-4 w-100" id="content" width="900" height="380">' +
+    '              <div class="d-flex justify-content-between">' +
+    '                 <div class="my-4 w-100" id="content" width="900" height="380">' +
     '                   {{{contents}}}                                                            ' +
     '                </div>\n' +
+    ' {{/data}}'+
+    ' {{#fileCheck}}'+
+    '                 <div class="dropdown">\n' +
+    '                           <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+                              '    첨부파일\n' +
+                              '  </button>\n' +
+                              '  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">\n' +
+    '                               {{#data.files}}   ' +
+                              '    <li><a class="dropdown-item" href="#">{{origFileName}} size:{{fileSize}}kb</a><li>\n' +
+    '                               {{/data.files}}   ' +
+
+                              '  </ul>\n' +
+                              '</div>' +
+    ' {{/fileCheck}}'+
+    '              </div>  ' +
     '\n' +
     '                <h2>Section title</h2>\n' +
     '                <div class="table-responsive">\n' +
     '\n' +
     '\n' +
     '                </div>\n' +
-    '            </main>' +
-    '' +
-    ' {{/data.CourseBoardData}}'
+    '            </main>'
+
+
+
+const templateNav ='' +
+    '{{#data}}' +
+    ' <li class="nav-item" s>\n' +
+    '                            <a role="button" class="courseboard-href nav-link" aria-current="page" data-id="{{id}}">\n' +
+    '                                {{title}}\n' +
+    '                            </a>\n' +
+    '                        </li>'+
+    ' {{/data}}'
 
 
 
 
-
-
-
+var courseBoard = getData();
 $(document).ready(function(){
-  let courseBoard = getCourseBoard();
+
+
   if (courseBoard.length > 0) {
     $('#viewBox').removeAttr("hidden");
-    let index = courseBoard[0].CourseBoardData.id;
-    render(index);
-    let k = $(".courseboard-href").first().addClass('active');
+    let index = courseBoard[0].id;
+    navRender(courseBoard);
+    mainRender(index,courseBoard);
+    $(".courseboard-href").first().addClass('active');
   } else {
 
     alert("생성된 강의 목차가없습니다. 강의 회차를 추가해주세요");
   }
-
 
 
 
@@ -51,7 +75,7 @@ $(document).ready(function(){
     let id = $(this).data('id');
     $(".courseboard-href").removeClass("active");
     $(this).addClass('active');
-    render(id);
+    mainRender(id,courseBoard);
   });
 
 
@@ -65,14 +89,46 @@ $(document).ready(function(){
 
 })
 
-function render(id) {
-  let courseBoard = getCourseBoard();
-  $('#content').empty();
-  Mustache.parse(template);
-  let content =courseBoard.filter(x => x.CourseBoardData.id === ''+id+'');
-  var jsonData = {
-    "data": content[0]
+function getData() {
+  var value;
+  var courseId =getCourseId();
+  $.ajax({
+    type: "get",
+    url: "/courseboard/data/"+courseId,
+    async: false,
+    success: function (data) {
+      value = data;
+    },
+    error: function (request, error) {
+      alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+      alert("오류");
+    }
+  });
+  return value;
+}
+
+function navRender(data){
+  let courseBoard = data;
+  $('.navResult').empty();
+  let jsonData = {
+    "data": courseBoard
   };
+  Mustache.parse(templateNav);
+  var randeredNav =Mustache.render(templateNav,jsonData);
+  $('#navResult').html(randeredNav);
+}
+
+function mainRender(id,data) {
+  let courseBoard = data;
+  $('#content').empty();
+  let content =courseBoard.filter(x => x.id === id);
+  let fileCheck =content[0].files.length == 0 ? null : true;
+  let jsonData = {
+    "data": content[0],
+    "fileCheck": fileCheck
+  };
+  Mustache.parse(template);
   var rendered = Mustache.render(template, jsonData);
   $('#result').html(rendered);
 }
+
--- src/main/resources/static/assets/dist/js/form-validation.js
+++ src/main/resources/static/assets/dist/js/form-validation.js
@@ -1,55 +1,74 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  'use strict'
+    'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-
-
+                form.classList.add('was-validated')
+            }, false)
+        })
 
 
 })()
+
+
+function formBtn() {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+    var check;
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          if (!form.checkValidity()) {
+            check = 1;
+          } else {
+            check= 2;
+          }
+            form.classList.add('was-validated');
+        })
+  return check;
+}
+
+
 function fileCheck(el) {
-  if(!/\.(jpeg|jpg|png|bmp)$/i.test(el.value)){
-    alert('이미지 파일만 업로드 가능합니다.');
-    el.value = '';
-    el.focus();
-  }
+    if (!/\.(jpeg|jpg|png|bmp)$/i.test(el.value)) {
+        alert('이미지 파일만 업로드 가능합니다.');
+        el.value = '';
+        el.focus();
+    }
 }
 
 
 function starFormCheck(form) {
-  let starForm=$(form).find('input:radio[name=star]').length;
-  let star = $(form).find('input:radio[name=star]').is(':checked');
-  let text = $(form).find('.form-control').val();
-  let textBoolean;
-  // 미입력 또는 공백 입력 방지
-  if (text.replace(/\s|　/gi, "").length == 0) {
-    $("#modal-message-text").focus();
-    textBoolean = false;
-  } else {
-    textBoolean = true;
-  }
+    let starForm = $(form).find('input:radio[name=star]').length;
+    let star = $(form).find('input:radio[name=star]').is(':checked');
+    let text = $(form).find('.form-control').val();
+    let textBoolean;
+    // 미입력 또는 공백 입력 방지
+    if (text.replace(/\s|　/gi, "").length == 0) {
+        $("#modal-message-text").focus();
+        textBoolean = false;
+    } else {
+        textBoolean = true;
+    }
 
 
-  if (starForm == 0) {
-    return textBoolean;
-  } else{
-    return star && textBoolean;
-  }
+    if (starForm == 0) {
+        return textBoolean;
+    } else {
+        return star && textBoolean;
+    }
 
 
 }
