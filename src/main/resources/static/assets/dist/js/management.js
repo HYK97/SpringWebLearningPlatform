22243efdf2d145d1bb4de9e02b013879234f5f81
ddh963963 <ddh963963@naver.com>
2022-03-19 21:09:39 +0900

    Feat : 첨부파일 기능 추가 및 view render 형식 model 에서 ajax 로변경
    기존의 컨트롤러에서 view model 로 가져오지않고 mustache를 이용해 ajax 통신후 데이터를 렌더링하도록함
    
    첨부파일 기능을 추가함

--- src/main/resources/static/assets/dist/js/management.js
+++ src/main/resources/static/assets/dist/js/management.js
@@ -1,29 +1,60 @@
 var fileCount = 0;
 // 해당 숫자를 수정하여 전체 업로드 갯수를 정한다.
+
 // 파일 고유넘버
 var fileNum = 0;
 // 첨부파일 배열
 var content_files = new Array();
 
 
-
-
 $(document).on("change", "#file", function () {
 
-
     var files = $('#file')[0].files;
-
-    // 파일 배열 담기
     var filesArr = Array.prototype.slice.call(files);
 
-    // 파일 개수 확인 및 제한
+    if (fileCount + filesArr.length > 10) {
+        alert('10개이상은 첨부하실수없습니다.');
+        return;
+    }
 
-    fileCount = fileCount + filesArr.length;
+
 
     // 각각의 파일 배열담기 및 기타
-    filesArr.forEach(function (f) {
+    /*for (let i = 0; i < filesArr.length; i++) {
+            if (filesArr[i].size > 10 * 1024 * 1024) {
+                alert(filesArr[i].name+'해당 파일크기가 너무큽니다. 업로드에서 제외됩니다.');
+                filesArr.splice(i);
+                break ;
+            }
         var reader = new FileReader();
         reader.onload = function (e) {
+            content_files.push(filesArr[i]);
+            $('#fileChange').append(
+                '<div id="file' + fileNum + '" onclick="fileDelete(\'file' + fileNum + '\')">'
+                + '<font style="font-size:12px">'+(i+1)+ filesArr[i].name + '</font>'
+                + '<svg style="width:20px; height:auto; vertical-align: middle; cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mx-1 bi bi-dash-square" viewBox="0 0 16 16">\n' +
+                '  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>\n' +
+                '  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>\n' +
+                '</svg>'
+                + '<div/>'
+            );
+            fileNum ++;
+        };
+        reader.readAsDataURL(filesArr[i]);
+    }*/
+
+
+    filesArr.forEach(function (f, index) {
+        if (f.size > 10 * 1024 * 1024) {
+            alert(f.name + '해당 파일크기가 너무큽니다. 업로드에서 제외됩니다.');
+            delete filesArr[index];
+        }
+    });
+
+
+    filesArr.forEach(function (f) {
+        var reader = new FileReader();
+        reader.onload = function (e,) {
             content_files.push(f);
             $('#fileChange').append(
                 '<div id="file' + fileNum + '" onclick="fileDelete(\'file' + fileNum + '\')">'
@@ -34,45 +65,42 @@ $(document).on("change", "#file", function () {
                 '</svg>'
                 + '<div/>'
             );
-            fileNum ++;
+            fileNum++;
+            fileCount++;
         };
         reader.readAsDataURL(f);
     });
     console.log(content_files);
 })
 
-// 파일 부분 삭제 함수
-function fileDelete(fileNum){
+function fileDelete(fileNum) {
     var no = fileNum.replace(/[^0-9]/g, "");
     content_files[no].is_delete = true;
     $('#' + fileNum).remove();
-    fileCount --;
+    fileCount--;
     console.log(content_files);
 }
 
-/*
- * 폼 submit 로직
- */
 
-
-
-
-
-
-
-$(document).ready(function(){
-    var courseId =getCourseId();
+$(document).ready(function () {
+    var courseId = getCourseId();
     $(document).on("click", "#createBtn", function () {
+        var check = formBtn();
+        if (check == 1) {
+            $("#title").focus();
+            return;
+        }
+        let title = $("#title").val();
+        let contents = $("#contents").val();
         var formData = new FormData();
 
 
-        formData.append('title',$("#title").val() );
-        formData.append('contents',$("#contents").val() );
+        formData.append('title', title);
+        formData.append('contents', contents);
 
 
         for (var x = 0; x < content_files.length; x++) {
-            // 삭제 안한것만 담아 준다.
-            if(!content_files[x].is_delete){
+            if (!content_files[x].is_delete) {
                 formData.append("file", content_files[x]);
             }
         }
@@ -80,12 +108,27 @@ $(document).ready(function(){
         $.ajax({
             type: "post",
             enctype: 'multipart/form-data',
-            url: "/courseboard/createBoard/"+courseId,
+            url: "/courseboard/createBoard/" + courseId,
             data: formData,
             processData: false,
             contentType: false,
             success: function (data) {
-                alert(data);
+                if (data == "1") {
+                    alert('등록성공');
+                    courseBoard = getData();
+                    navRender(courseBoard);
+                    $(".courseboard-href").removeClass("active");
+                    $(".courseboard-href").last().trigger("click");
+                    $('#createBox').attr("hidden", "hidden");
+                    $('#viewBox').removeAttr("hidden");
+                    $("form")[0].reset();
+                    $("form").removeClass("was-validated");
+                    $('#fileChange').empty();
+                    $('#contents').summernote('reset');
+
+                } else {
+                    alert('오류');
+                }
             },
             error: function (request, error) {
                 alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
