6fb0e76124a5f2d1f6d9bf41642c299bc11af827
ddh963963 <ddh963963@naver.com>
2022-03-23 17:45:03 +0900

    Feat : courseBoard 댓글 view 추가

--- src/main/resources/static/assets/dist/js/courseboard.js
+++ src/main/resources/static/assets/dist/js/courseboard.js
@@ -1,3 +1,75 @@
+/*
+const temp =
+    '        <div class="row my-1 ">\n' +
+    '            <div >\n' +
+    '                <div class="text-break">\n' +
+    '                    <div >\n' +
+    '                        <div class="user-field">\n' +
+    '                            <div class="mx-2">\n' +
+    '                                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32"\n' +
+    '                                     class="rounded-circle">\n' +
+    '                            </div>\n' +
+    '                            <div class="user-field-name">\n' +
+    '                                <div> <p class=" my-3 ">{{username}}</p></div>\n' +
+    '               {{#teachUser}}{{^reply}} ' +
+    '                <div class="dropdown dropdown-user text-end" hidden data-user="{{username}}">\n' +
+    '                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+    '                </a>\n' +
+    '                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">\n' +
+    '                    <li><a class="dropdown-item createReply" data-bs-toggle="modal" data-id="{{id}}" data-bs-target="#exampleModal3">답글쓰기</a></li>\n' +
+    '                </ul>{{/reply}}{{/teachUser}}\n' +
+
+    '               {{^teachUser}}' +
+    '                <div class="dropdown dropdown-user text-end" hidden data-user="{{username}}">\n' +
+    '                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+    '                </a>\n' +
+    '                      <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">\n' +
+    '                    <li><a class="dropdown-item update" data-bs-toggle="modal" data-scope="{{scope}}" data-comments="{{comments}}" data-id="{{id}}"data-bs-target="#exampleModal" >수정</a></li>\n' +
+    '                    <li><a class="dropdown-item delete" data-id="{{id}}">삭제</a></li>\n' +
+    '                </ul>{{/teachUser}}\n' +
+    '            </div>\n' +
+    '                            </div>\n' +
+    '                        </div>\n' +
+    '                        <p class="text-muted my-2 comments">\n' +
+    '                            {{comments}}\n' +
+    '                        </p>\n' +
+    '\n' +
+    '                        <small class=" my-3 create-date">\n' +
+    '                            {{createDate}}\n' +
+    '                        </small>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            {{#reply}}\n' +
+    '                <div class="reply-box  text-break">\n' +
+    '                            <div class="user-field-name">\n' +
+    '                                <div> <p class=" my-3"> 강사 ' + getId().teachName + '님의 답글</p></div>\n' +
+    '                   {{#teachUser}}\n' +
+    '                                <div class="dropdown text-end">\n' +
+    '                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+    '                </a>\n' +
+    '                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">\n' +
+    '                    <li><a class="dropdown-item update" data-id="{{replyId}}" data-comments="{{reply}}" data-bs-toggle="modal" data-bs-target="#exampleModal2">수정</a></li>\n' +
+    '                    <li><a class="dropdown-item delete" data-id="{{replyId}}" >삭제</a></li>\n' +
+    '                </ul>\n' +
+    '                            </div>\n' +
+    '                        {{/teachUser}}\n' +
+    '                        </div>\n' +
+    '                    <p class="text-muted my-2 ">\n' +
+    '                        {{reply}}\n' +
+    '                    </p>\n' +
+    '                     <div>  \n' +
+    '                    <small class=" my-3 create-date">\n' +
+    '                        {{createDate}}\n' +
+    '                    </small>\n' +
+    '                    </div> \n' +
+    '                </div>\n' +
+    '            {{/reply}}\n' +
+    '            <hr class=" mt-3 mb-2">\n' +
+    '        </div>\n'
+*/
+
+
 const template = ' ' +
     ' {{#data}}' +
     '<main class="col-md-12 ms-sm-auto col-lg-12  px-md-4">\n' +
@@ -31,12 +103,71 @@ const template = ' ' +
     ' {{/fileCheck}}' +
     '              </div>  ' +
     '\n' +
-    '                <h2>Section title</h2>\n' +
-    '                <div class="table-responsive">\n' +
+    '                <div class="my-5 col-12">\n' +
+    '                <h5>댓글</h5>\n' +
     '\n' +
-    '\n' +
+    '                <div class="my-3 col-12 border p-3 border-1">\n' +
+    '                <form id="commentsForm">\n' +
+    '                <input type="text" class="form-control comments" placeholder="댓글을 입력하세요" name="comments">\n' +
+    '                </form>\n' +
+    '                           <button class="btn btn-secondary mt-3" type="button" id ="commentsCreateBtn">\n' +
+    '                                     댓글쓰기\n' +
+    '                            </button>\n' +
+    '                </div>\n' +
+    '                </div>\n' +
+    '                <div class="my-5 col-12 ">\n' +
+    '                <div class="my-3 col-12" id="commentsList">\n' +
+    '                </div>\n' +
     '                </div>\n' +
     '            </main>'
+
+
+const comments = '' +
+    '            <hr class=" mt-3 mb-2">\n' +
+    ' {{#data}}' +
+    '        <div class="row my-1 ">\n' +
+    '            <div >\n' +
+    '                <div class="text-break">\n' +
+    '                    <div >\n' +
+    '                        <div class="user-field">\n' +
+    '                            <div class="mx-2">\n' +
+    '                                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32"\n' +
+    '                                     class="rounded-circle">\n' +
+    '                            </div>\n' +
+    '                            <div class="user-field-name">\n' +
+    '                                <div> <p class=" my-3 ">{{username}}</p></div>\n' +
+    '               {{^myCommentsFlag}} ' +
+    '                <div class="dropdown dropdown-user text-end" data-user="{{username}}">\n' +
+    '                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+    '                </a>\n' +
+    '                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">\n' +
+    '                    <li><a class="dropdown-item createReply">답글쓰기</a></li>\n' +
+    '                </ul>{{/myCommentsFlag}}\n' +
+
+    '               {{#myCommentsFlag}}' +
+    '                <div class="dropdown dropdown-user text-end"  data-user="{{username}}">\n' +
+    '                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">\n' +
+    '                </a>\n' +
+    '                      <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">\n' +
+    '                    <li><a class="dropdown-item update"  >수정</a></li>\n' +
+    '                    <li><a class="dropdown-item delete" data-id="{{id}}">삭제</a></li>\n' +
+    '                </ul>{{/myCommentsFlag}}\n' +
+    '            </div>\n' +
+    '                            </div>\n' +
+    '                        </div>\n' +
+    '                        <p class="text-muted my-2 comments">\n' +
+    '                            {{comments}}\n' +
+    '                        </p>\n' +
+    '\n' +
+    '                        <small class=" my-3 create-date">\n' +
+    '                            {{createDate}}\n' +
+    '                        </small>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            </div>\n' +
+    '            <hr class=" mt-3 mb-2">\n' +
+    ' {{/data}}'
 
 
 const templateNav = '' +
@@ -74,8 +205,6 @@ $(document).ready(function () {
         $(this).addClass('active');
         mainRender(id, courseBoard);
     });
-
-
 
 
 })
@@ -141,9 +270,11 @@ function navRender(data) {
 
 function mainRender(id, data) {
     let courseBoard = data;
-    var someTabTriggerEl = document.querySelector('#main-tab');
-    var tab = new bootstrap.Tab(someTabTriggerEl);
-    tab.show();
+    if (document.querySelector('#main-tab') != null) {
+        var someTabTriggerEl = document.querySelector('#main-tab');
+        var tab = new bootstrap.Tab(someTabTriggerEl);
+        tab.show();
+    }
     $('#content').empty();
     let content = courseBoard.filter(x => x.id === id);
     let fileCheck = content[0].files.length == 0 ? null : true;
@@ -157,6 +288,59 @@ function mainRender(id, data) {
     var rendered = Mustache.render(template, jsonData);
     $('#result').html(rendered);
     courseBoardId = content[0].id;
+    commentsRender();
 }
 
+function commentsRender() {
+    $.ajax({
+        type: "get",
+        url: '/comments/getComments/'+courseBoardId,
+        success: function (data) {
+            let jsonData = {
+                "data": data,
+            };
+            Mustache.parse(comments);
+            var rendered = Mustache.render(comments, jsonData);
+            $('#commentsList').html(rendered);
+        },
+        error: function (request, error) {
+            alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+            alert("오류");
+        }
+    })
+
+
+
+}
+
+
+
+$(document).on('click','#commentsCreateBtn',function () {
+    let comments = $("input[name=comments]").val();
+    if(comments.length == 0) {
+        alert("댓글을 작성후 눌러주세요");
+        return;
+    }
+    $.ajax({
+        type: "post",
+        url: "/comments/create/" + courseBoardId,
+        data : {
+            comments : comments
+        },
+        success: function (data) {
+            if (data == "1") {
+                alert("댓글 작성 성공");
+                $("input[name=comments]").val("");
+                commentsRender();
+            } else {
+                alert("실패");
+            }
+        },
+        error: function (request, error) {
+            alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+            alert("오류");
+        }
+    });
+
+})
 
