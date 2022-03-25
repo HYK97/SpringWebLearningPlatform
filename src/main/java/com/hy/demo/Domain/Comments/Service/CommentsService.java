66005b861c4a71b90bbbcc5fb092108c18ea5334
ddh963963 <ddh963963@naver.com>
2022-03-25 23:37:20 +0900

    Feat : commentsService 추가
    Controller에 필요한 댓글을 저장하거나 불러올때 필요한 메소드를 정의함

---
+++ src/main/java/com/hy/demo/Domain/Comments/Service/CommentsService.java
@@ -0,0 +1,96 @@
+package com.hy.demo.Domain.Comments.Service;
+
+import com.hy.demo.Domain.Board.Entity.CourseBoard;
+import com.hy.demo.Domain.Board.Repository.CourseBoardRepository;
+import com.hy.demo.Domain.Comments.Dto.CommentsDto;
+import com.hy.demo.Domain.Comments.Entity.Comments;
+import com.hy.demo.Domain.Comments.Repository.CommentsRepository;
+import com.hy.demo.Domain.User.Entity.User;
+import com.hy.demo.Domain.User.Repository.UserRepository;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+/**
+ * service 명명 규칙
+ * select -> find
+ * modifyCourseEvaluation -> modify
+ * insert -> add
+ * delete -> delete
+ */
+
+@Service
+public class CommentsService {
+    @Autowired
+    private CourseBoardRepository courseBoardRepository;
+
+    @Autowired
+    private CommentsRepository commentsRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Autowired
+    Logger logger;
+
+    public void createComments(Long courseId, String comments, User user) {
+        CourseBoard courseBoard = courseBoardRepository.findById(courseId)
+                .orElseThrow(() -> new EntityNotFoundException("찾는 courseBoard없음"));
+
+        User findUser = Optional.ofNullable(userRepository.findByUsername(user.getUsername()))
+                .orElseThrow(() -> new EntityNotFoundException("찾는 유저 없음"));
+
+        Comments comment = Comments.builder()
+                .comments(comments)
+                .user(findUser)
+                .courseBoard(courseBoard)
+                .build();
+        commentsRepository.save(comment);
+    }
+
+    public CommentsDto createReply(Long commentsId, Long courseId, String comments, User user) {
+        CourseBoard courseBoard = courseBoardRepository.findById(courseId)
+                .orElseThrow(() -> new EntityNotFoundException("찾는 courseBoard없음"));
+
+        User findUser = Optional.ofNullable(userRepository.findByUsername(user.getUsername()))
+                .orElseThrow(() -> new EntityNotFoundException("찾는 유저 없음"));
+
+        Comments comment = commentsRepository.findById(commentsId)
+                .orElseThrow(() -> new EntityNotFoundException("찾는 댓글없음"));
+
+        Comments reply = Comments.builder()
+                .parent(comment)
+                .comments(comments)
+                .user(findUser)
+                .courseBoard(courseBoard)
+                .build();
+        Comments replyEntity = commentsRepository.save(reply);
+        CommentsDto commentsDto = replyEntity.changeDto();
+        commentsDto.setMyCommentsFlag(1);
+        commentsDto.setReplyId(comment.getId());
+        commentsDto.setUsername(findUser.getUsername());
+        return commentsDto;
+    }
+
+
+    public Page<CommentsDto> findCommentsListByCourseId(Long courseId, Pageable pageable) {
+        Page<CommentsDto> commentsList = commentsRepository.findByCourseBoardId(courseId, pageable);
+        for (CommentsDto commentsDto : commentsList.getContent()) {
+            logger.info("commentsList = " + commentsDto.toString());
+        }
+        return commentsList;
+    }
+
+    public Page<CommentsDto> findReplyListByCommentsId(Long commentsId, Pageable pageable) {
+        Page<CommentsDto> replyByIds = commentsRepository.findReplyByIds(commentsId, pageable);
+        return replyByIds;
+    }
+
+
+}
