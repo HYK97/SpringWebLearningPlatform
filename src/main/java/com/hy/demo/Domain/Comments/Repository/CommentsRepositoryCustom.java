89fe664738e5b6572369441fc0026348782c172b
ddh963963 <ddh963963@naver.com>
2022-03-28 17:32:06 +0900

    Feat : 댓글 권한 확인용 findByIdUser 메소드 추가
    수정 및 삭제에 필요한 데이터 권한 확인용 메서드
    각 댓글 id 와 user의 일치 확인

--- src/main/java/com/hy/demo/Domain/Comments/Repository/CommentsRepositoryCustom.java
+++ src/main/java/com/hy/demo/Domain/Comments/Repository/CommentsRepositoryCustom.java
@@ -1,12 +1,14 @@
 package com.hy.demo.Domain.Comments.Repository;
 
 import com.hy.demo.Domain.Comments.Dto.CommentsDto;
+import com.hy.demo.Domain.Comments.Entity.Comments;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface CommentsRepositoryCustom {
     Page<CommentsDto> findByCourseBoardId(Long courseBoardId, Pageable pageable);
     Page<CommentsDto> findReplyByIds(Long id, Pageable pageable);
+    Optional<Comments> findByIdAndUser(Long id, String username);
 }
