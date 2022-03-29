5867aa7e7bbdd7b942f08a90a791cbd435f49860
ddh963963 <ddh963963@naver.com>
2022-03-29 19:16:28 +0900

    Feat : 회원정보 view 페이지 추가

---
+++ src/main/resources/static/assets/dist/js/userinfo.js
@@ -0,0 +1,8 @@
+$(document).on('click', '.user', function () {
+
+    location.href ="/user/info";
+});
+$(document).on('click', '.security', function () {
+
+    location.href ="/user/security";
+});
