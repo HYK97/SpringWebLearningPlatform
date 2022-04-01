13ec68d4411e2f4613d71f84ff8ff2026a61428c
ddh963963 <ddh963963@naver.com>
2022-04-01 21:54:13 +0900

    Feat : file data modal 에 띄우도록 설정

--- src/main/resources/static/assets/dist/js/userProfileCropper.js
+++ src/main/resources/static/assets/dist/js/userProfileCropper.js
@@ -1,55 +1,18 @@
-$(function(){
-    var cropper;
-    // 사진 업로드 버튼
-    $('#photoBtn').on('change', function(){
-        $('.them_img').empty().append('<img id="image" src="">');
-        var image = $('#image');
-        var imgFile = $('#photoBtn').val();
-        var fileForm = /(.*?)\.(jpg|jpeg|png)$/;
+var cropper;
+$(function () {
 
-        // 이미지가 확장자 확인 후 노출
-        if(imgFile.match(fileForm)) {
-            var reader = new FileReader();
-            reader.onload = function(event) {
 
-                image.attr("src", event.target.result);
-                cropper = image.cropper( {
-                    dragMode: 'move',
-                    viewMode:1,
-                    aspectRatio: 1,
-                });
-            };
-            reader.readAsDataURL(event.target.files[0]);
-            $('#cropperModal').modal('show');
-        } else{
-            alert("이미지 파일(jpg, png형식의 파일)만 올려주세요");
-            $('#photoBtn').focus();
-            return;
-        }
-    });
-    // 사진 다시 올리기 버튼
-    $('#resetPhoto').on('click', function(){
-        if($('input[type="file"]').val() != ""){
-            $('#photoBtn').val('');
-            $('.them_img img').attr('src','').remove();
-            $('.btn_wrap a:last-child').removeClass('bg1');
-            $('input[type="file"]').click();
-        }else{
-            //alert('업로드된 사진이 없습니다.');
-        }
-    });
-    // 업로드 버튼
-    $('#complete').on('click', function(){
+    $('#complete').on('click', function () {
         $('.them_img').append('<div class="result_box"><img id="result" src=""></div>');
         var image = $('#image');
         var result = $('#result');
         var canvas;
-        if($('input[type="file"]').val() != ""){
-            canvas = image.cropper('getCroppedCanvas',{
-                width:200,
-                height:200
+        if ($('input[type="file"]').val() != "") {
+            canvas = image.cropper('getCroppedCanvas', {
+                width: 500,
+                height: 500
             });
-            result.attr('src',canvas.toDataURL("image/jpg"));
+            result.attr('src', canvas.toDataURL("image/jpg"));
 
             canvas.toBlob(function (blob) {
                 var formData = new FormData();
@@ -68,14 +31,53 @@ $(function(){
                     },
                 });
             })
-        }else{
+        } else {
             alert('사진을 업로드 해주세요');
             $('input[type="file"]').focus();
             return;
         }
     });
 });
+
 function onClickUpload() {
-    let myInput = document.getElementById("photoBtn");
-    myInput.click();
+    let myInput = $("#photoBtn");
+    $("#photoBtn").val('');
+    myInput.trigger('click');
+
 }
+
+$('.modal-close').on('click', function () {
+    $('#cropperModal').modal("hide"); //닫기
+});
+
+$('#photoBtn').on('change', function () {
+    $('.them_img').empty().append('<img id="image" src="">');
+    var image = $('#image');
+    var imgFile = $('#photoBtn').val();
+    var fileForm = /(.*?)\.(jpg|jpeg|png)$/;
+
+    if (imgFile.match(fileForm)) {
+        var reader = new FileReader();
+        reader.onload = function () {
+
+            image.attr("src", event.target.result);
+
+            cropper = image.cropper({
+                dragMode: 'move',
+                viewMode: 1,
+                aspectRatio: 1,
+                minContainerWidth: 470,
+                minContainerHeight: 500
+            });
+
+
+        };
+        reader.readAsDataURL(event.target.files[0]);
+        $('#cropperModal').modal("show");
+    } else {
+        alert("이미지 파일(jpg, png형식의 파일)만 올려주세요");
+        $('#photoBtn').focus();
+        return;
+    }
+});
+
