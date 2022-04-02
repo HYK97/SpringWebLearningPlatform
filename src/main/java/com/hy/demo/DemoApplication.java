70967ae900b3f98e121a80c6c4ac9d63c335f2f6
ddh963963 <ddh963963@naver.com>
2022-04-02 17:26:51 +0900

    Feat : profileImage 도메인 추가, profile 데이터 저장 및 이미지 저장 경로 변경

--- src/main/java/com/hy/demo/DemoApplication.java
+++ src/main/java/com/hy/demo/DemoApplication.java
@@ -6,18 +6,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class DemoApplication {
-///a7d22101-707e-4eff-9994-5f26983d5520
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@Bean(name = "uploadPath")
 	public String uploadPath() {
-		return "D:/image/";
+		LocalDate now = LocalDate.now();
+		String Path = "resources/image/" + now;
+		return Path;
 	}
 
-	@Bean Logger logger () {
+	@Bean
+	Logger logger () {
 		return LoggerFactory.getLogger(this.getClass());
 	}
 }
