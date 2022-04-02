b199e67d02eb48a2909122548157f751564d13aa
ddh963963 <ddh963963@naver.com>
2022-04-02 17:43:23 +0900

    Refactor : profileImage 로드 변경 및 courseBoard document ready 메소드 변경
    각 페이지가 렌더링 되는 곳에서 controller가 넘겨준 profileImage 경로를 통해서 이미지를 보여주도록 변경 및
    management 및 courseboard js에서 각 courseBoard 데이터가 없을시에대한 알림 설정을 수정

--- src/main/resources/static/assets/dist/js/header.js
+++ src/main/resources/static/assets/dist/js/header.js
@@ -2,15 +2,22 @@ $(document).ready(function() {
 
     $.ajax({
         type : "post",
-        url : "/user/role",
+        url : "/user/getUser",
         cache : false,
         success : function(data){
-            if (data == 'ROLE_USER') {
-                $('#manager').remove();
-            } else {
-                $('#user').after('<li><a id="manager" class="dropdown-item" href="/course/createview">강의 생성</a></li>');
-                $('#user').after('<li><a id="manager" class="dropdown-item" href="/course/info/mycourselist">강의 관리</a></li>');
+            if (data != null) {
+                if (data.role == 'ROLE_USER') {
+                    $('#manager').remove();
+                } else {
+                    $('#user').after('<li><a id="manager" class="dropdown-item" href="/course/createview">강의 생성</a></li>');
+                    $('#user').after('<li><a id="manager" class="dropdown-item" href="/course/info/mycourselist">강의 관리</a></li>');
+                }
+                if (data.profileImage != null) {
+                    $("#header-profileImage").attr('src',data.profileImage);
+                }
 
+            }else {
+                alert("세션종료");
             }
         },
         error:function(request, error) {
