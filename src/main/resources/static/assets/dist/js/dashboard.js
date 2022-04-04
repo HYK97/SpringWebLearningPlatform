48b5e16e485345c30a9bd965939a4dbf0f6a58db
ddh963963 <ddh963963@naver.com>
2022-04-04 19:05:29 +0900

    Refactor : dashboard 상단 4개 카드 데이터 연결 및 표시

--- src/main/resources/static/assets/dist/js/dashboard.js
+++ src/main/resources/static/assets/dist/js/dashboard.js
@@ -1,40 +1,79 @@
-var chart1=null;
-var chart2=null;
-var chart3=null;
-$(document).on("click",'#statisticsBtn',function () {
+var chart1 = null;
+var chart2 = null;
+var chart3 = null;
+$(document).on("click", '#statisticsBtn', function () {
+    $("#percentUser").empty();
+    $("#percentScope").empty();
+    $("#percentComment").empty();
     statisticsBoxShow();
+    getDashboardData();
     $('.counter').counterUp({
         delay: 10,
-        time: 1500
+        time: 500
     });
 
+    drawChart1();
+    drawChart2();
+    drawChart3();
+});
+
+function getDashboardData() {
+    $.ajax({
+        url: '/courseboard/getDashBoard/' + getCourseId(),
+        type: 'post',
+        async: false,
+        success: function (data) {
+            var userPercent = data.todayRegisteredUser - data.yesterdayRegisteredUser;
+            var scopePercent =  data.todayScope-data.yesterdayScope;
+            var commentPercent = data.todayComment - data.yesterdayComment
+            $("#todayUser").text(data.todayRegisteredUser);
+
+
+
+            $("#todayScope").text(data.todayScope);
+            $("#todayComment").text(data.todayComment);
+            $("#allView").text(data.allView);
+
+
+            userPercent >= 0 ? $("#percentUser").append('어제대비 <span class="text-success text-sm font-weight-bolder">+' + userPercent + '명 </span>') : $("#percentUser").append('어제 대비 <span class="text-danger text-sm font-weight-bolder">' + userPercent + '명 </span>')
+            scopePercent >= 0 ? $("#percentScope").append('어제대비 <span class="text-success text-sm font-weight-bolder">' + scopePercent.toFixed(1) + ' 점 증가 </span>') : $("#percentScope").append('어제 대비 <span class="text-danger text-sm font-weight-bolder">"' + scopePercent.toFixed(1) + '"점 감소 </span>')
+            commentPercent >= 0 ? $("#percentComment").append('어제대비 <span class="text-success text-sm font-weight-bolder">+' + commentPercent + '개 </span>') : $("#percentComment").append('어제 대비 <span class="text-danger text-sm font-weight-bolder">' + commentPercent + '개 </span>')
+        },
+        error: function (request, error) {
+            alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+            alert("오류");
+        }
+    });
+}
+
+function drawChart1() {
     var ctx = document.getElementById("chart-bars").getContext("2d");
-    var ctx2 = document.getElementById("chart-line").getContext("2d");
-    var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
     if (chart1 != null) {
         window.chart1.destroy();
-        window.chart2.destroy();
-        window.chart3.destroy();
     }
-
-    chart1=new Chart(ctx, {
-        type: "bar",
+    chart1 = new Chart(ctx, {
+        type: "line",
         data: {
-            labels: ["월", "화", "수", "목", "금", "토", "일"],
+            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
             datasets: [{
-                label: "Sales",
-                tension: 0.4,
+                label: "Mobile apps",
+                tension: 0,
                 borderWidth: 0,
-                borderRadius: 4,
-                borderSkipped: false,
-                backgroundColor: "rgba(255, 255, 255, .8)",
-                data: [50, 20, 10, 22, 50, 10, 40],
+                pointRadius: 5,
+                pointBackgroundColor: "rgba(255, 255, 255, .8)",
+                pointBorderColor: "transparent",
+                borderColor: "rgba(255, 255, 255, .8)",
+                borderWidth: 4,
+                backgroundColor: "transparent",
+                fill: true,
+                data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
                 maxBarThickness: 6
-            },],
+
+            }],
         },
         options: {
             animation: {
-                duration: 3000,
+                duration: 1000,
             },
             responsive: true,
             maintainAspectRatio: false,
@@ -58,10 +97,9 @@ $(document).on("click",'#statisticsBtn',function () {
                         color: 'rgba(255, 255, 255, .2)'
                     },
                     ticks: {
-                        suggestedMin: 0,
-                        suggestedMax: 500,
-                        beginAtZero: true,
+                        display: true,
                         padding: 10,
+                        color: '#f8f9fa',
                         font: {
                             size: 14,
                             weight: 300,
@@ -69,17 +107,15 @@ $(document).on("click",'#statisticsBtn',function () {
                             style: 'normal',
                             lineHeight: 2
                         },
-                        color: "#fff"
-                    },
+                    }
                 },
                 x: {
                     grid: {
                         drawBorder: false,
-                        display: true,
-                        drawOnChartArea: true,
+                        display: false,
+                        drawOnChartArea: false,
                         drawTicks: false,
-                        borderDash: [5, 5],
-                        color: 'rgba(255, 255, 255, .2)'
+                        borderDash: [5, 5]
                     },
                     ticks: {
                         display: true,
@@ -97,11 +133,14 @@ $(document).on("click",'#statisticsBtn',function () {
             },
         },
     });
+}
 
-
-
-
-    chart2=new Chart(ctx2, {
+function drawChart2() {
+    var ctx2 = document.getElementById("chart-line").getContext("2d");
+    if (chart2 != null) {
+        window.chart2.destroy();
+    }
+    chart2 = new Chart(ctx2, {
         type: "line",
         data: {
             labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
@@ -124,7 +163,7 @@ $(document).on("click",'#statisticsBtn',function () {
         },
         options: {
             animation: {
-                duration: 3000,
+                duration: 1000,
             },
             responsive: true,
             maintainAspectRatio: false,
@@ -185,8 +224,17 @@ $(document).on("click",'#statisticsBtn',function () {
         },
     });
 
+}
 
-    chart3=new Chart(ctx3, {
+function drawChart3() {
+    var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
+
+    if (chart3 != null) {
+        window.chart3.destroy();
+    }
+
+
+    chart3 = new Chart(ctx3, {
         type: "line",
         data: {
             labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
@@ -208,7 +256,7 @@ $(document).on("click",'#statisticsBtn',function () {
         },
         options: {
             animation: {
-                duration: 3000,
+                duration: 1000,
             },
             responsive: true,
             maintainAspectRatio: false,
@@ -268,4 +316,4 @@ $(document).on("click",'#statisticsBtn',function () {
             },
         },
     });
-});
+}
