9ffebf993715773bedae641fc576aab1a9e5b5f6
ddh963963 <ddh963963@naver.com>
2022-04-06 15:00:19 +0900

    Refactor : DateFormatter constructor 매개변수 변경

--- src/main/java/com/hy/demo/Utils/DateFormater.java
+++ src/main/java/com/hy/demo/Utils/DateFormater.java
@@ -12,24 +12,54 @@ import java.time.format.DateTimeFormatter;
 public class DateFormater {
     private LocalDate searchDate;
 
-    public DateFormater(String currentDate,String flag) {
+    public DateFormater(String currentDate) {
 
         this.searchDate = LocalDate.parse(currentDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
     }
 
     public Timestamp startDate() {
-        return Timestamp.valueOf(LocalDateTime.of(this.searchDate, LocalTime.of(0,0,0)));
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate, LocalTime.of(0, 0, 0)));
 
     }
+
     public Timestamp endDate() {
-        return Timestamp.valueOf(LocalDateTime.of(this.searchDate, LocalTime.of(23,59,59)));
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate, LocalTime.of(23, 59, 59)));
     }
+
 
     public Timestamp startMonth() {
-        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear(),this.searchDate.getMonth(),1,0,0,0));
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear(), this.searchDate.getMonth(), 1, 0, 0, 0));
     }
+
     public Timestamp endMonth() {
-        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear(),this.searchDate.getMonth(),this.searchDate.lengthOfMonth(),23,59,59));
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear(), this.searchDate.getMonth(), this.searchDate.lengthOfMonth(), 23, 59, 59));
     }
+
+    public Timestamp thisYearStart() {
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear(), 1, 1, 0, 0, 0));
+    }
+
+    public Timestamp thisYearEnd() {
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear(), 12, this.searchDate.lengthOfMonth(), 23, 59, 59));
+    }
+
+    public Timestamp tenYearAgo() {
+        return Timestamp.valueOf(LocalDateTime.of(this.searchDate.getYear() - 10, 1, 1, 0, 0, 0));
+    }
+
+
+    public int getYear() {
+        return this.searchDate.getYear();
+    }
+
+    public String getMonth() {
+        return this.searchDate.format(DateTimeFormatter.ofPattern("MM"));
+    }
+
+    public int endDay() {
+        return this.searchDate.lengthOfMonth();
+    }
+
+
 }
