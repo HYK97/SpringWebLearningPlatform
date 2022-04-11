97b9c6546cb40729b8dcc207d0b0830c052254c2
ddh963963 <ddh963963@naver.com>
2022-04-11 19:15:45 +0900

    Refactor : 메인 페이지 username -> nickname 으로 변경

--- src/main/java/com/hy/demo/Domain/Course/Dto/CourseUser.java
+++ src/main/java/com/hy/demo/Domain/Course/Dto/CourseUser.java
@@ -8,15 +8,18 @@ import lombok.Data;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 
 public class CourseUser {
-    public CourseUser(String username, String email, String role) {
+    public CourseUser(String username, String email, String role,String nickname) {
         this.username = username;
         this.email = email;
         this.role = role;
+        this.nickname = nickname;
     }
 
     private String username;
-        private String email;
-        private String role;
+    private String email;
+    private String role;
+    private String nickname;
+
 
 
 }
