c7df331669b18028515c5780f73099cfe3a41153
ddh963963 <ddh963963@naver.com>
2022-05-15 17:06:40 +0900

    Refactor : MainController, Domain-Board injection 생성자로 변경

--- src/main/java/com/hy/demo/Controller/MainController.java
+++ src/main/java/com/hy/demo/Controller/MainController.java
@@ -6,6 +6,7 @@ import com.hy.demo.Domain.Course.Service.CourseService;
 import com.hy.demo.Domain.User.Dto.UserDto;
 import com.hy.demo.Domain.User.Service.UserService;
 import com.hy.demo.Utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,30 +22,30 @@ import java.util.List;
 @Controller
 
 @RequestMapping("/main/*")
+@RequiredArgsConstructor
 public class MainController {
 
 
-    @Autowired
-    private CourseService courseService;
 
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private CourseEvaluationService courseEvaluationService;
+    private final CourseService courseService;
+
+    private final UserService userService;
+
+    private final CourseEvaluationService courseEvaluationService;
 
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger;
 
     @GetMapping({"/index"})
     public String index(Model model) {
 
-        List<UserDto> userList = userService.rankRandomUser(3);
-        List<CourseDto> headerView = courseService.randomCourseList(5);
-        List<CourseDto> firstCourse = courseService.findRankingScopeAvgCourse(1);
-        List<CourseDto> secondCourse = courseService.findRankingEvaluationCountCourse(1);
-        List<CourseDto> thirdCourse = courseService.findRankingUserCourseCountCourse(1);// 쿼리 분리
+        final List<UserDto> userList = userService.rankRandomUser(3);
+        final List<CourseDto> headerView = courseService.randomCourseList(5);
+        final List<CourseDto> firstCourse = courseService.findRankingScopeAvgCourse(1);
+        final List<CourseDto> secondCourse = courseService.findRankingEvaluationCountCourse(1);
+        final List<CourseDto> thirdCourse = courseService.findRankingUserCourseCountCourse(1);// 쿼리 분리
         if (!ObjectUtils.isEmpty(thirdCourse)) {
-            Double avgScopeByCourseId = courseEvaluationService.findAvgScopeByCourseId(thirdCourse.get(0).getId());
+            final Double avgScopeByCourseId = courseEvaluationService.findAvgScopeByCourseId(thirdCourse.get(0).getId());
             thirdCourse.get(0).updateScope(avgScopeByCourseId);
             model.addAttribute("thirdCourse", thirdCourse);
         }
